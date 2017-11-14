05cfdf69fe0166d97644160df4e5c48d0b916ffd
Tom Brierley <tom.brierley@carterdigital.com.au>
2017-11-15 08:51:36 +1100

    FIX: Resolve bugs

--- src/index.js
+++ src/index.js
@@ -11,20 +11,17 @@ import registerServiceWorker from './utilities/registerServiceWorker';
 // Define app container
 const MOUNT_NODE = document.getElementById('root');
 
-
 // Render app
 const render = () => {
   if (!MOUNT_NODE) return;
-  ReactDOM.render(
-      <App />
-    MOUNT_NODE,
-  );
+  ReactDOM.render(<App />, MOUNT_NODE);
 };
 
 render();
 registerServiceWorker();
 
-// Turn on hot module reloading - Refreshes the page content on code change without reloading the page.
+// Turn on hot module reloading
+// Refreshes the page content on code change without reloading the page.
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
